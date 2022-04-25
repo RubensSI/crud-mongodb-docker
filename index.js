51327135e12951b5fec11bdeb95f98f85c32b5f9
RubensSI <rubenssi825@gmail.com>
2022-04-24 22:43:11 -0300

    conexão com mongodb

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const PORT = 3000
-const HOST = '0.0.0.0'
+require('./models/Metas');
+const Meta = mongoose.model('Meta');
+
+const PORT = 3000;
+const HOST = "0.0.0.0";
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ massage : 'Hello, world!' });
-})
+mongoose
+  .connect("mongodb://db:27017/crud-node-mongo-docker", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Conexão com BD MongoDB realizada com sucesso");
+  })
+  .catch((err) => {
+    console.log(
+      `Erro: Conexão com BD MongoDB não realizada com sucesso ${err}`
+    );
+  });
 
-app.listen(PORT, HOST, () => console.log(`Server ativo na porta ${PORT}`))
+app.get("/metas", async (req, res) => {
+  return res.json({ description: "Aprender a desenvolver apis em node" });
+});
+
+app.post("/metas", async (req, res) => {
+  await 
+  return res.json({
+    error: false,
+    metas: req.body
+  });
+});
+
+app.listen(PORT, HOST, () => console.log(`Server ativo na porta ${PORT}`));
